e2e05e73250a7c27552c63755139348bee095dbf
shhadr <shahadahmad18@gmail.com>
2019-08-21 22:56:13 +0300

    4+5

---
+++ Day4+5.js
@@ -0,0 +1,7 @@
+// Hello, this is my first challenge and I will do it 10 times.
+// doc
+// I will do it, I will learn JavaScript.
+var num = 10;
+document.write("Hello, this is my first challenge and I will do it " + num + " times.")
+document.write("<br>")
+document.write("I will do it, I will learn JavaScript.") // yes i will do!
