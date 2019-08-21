cd112905f6e6367786cc6cce280c497787623dd2
shhadr <shahadahmad18@gmail.com>
2019-08-21 22:28:48 +0300

    third comment

---
+++ day2.js
@@ -0,0 +1,10 @@
+//This is my first code
+
+document.write("مرحباً بك في عالم البرمجة! ");
+
+/* This is my first code And I need to witre more comments 😊 
+document.write(“النص هنا لايتم طباعته لانه داخل التعليقات ”) */
+
+document.write("مرحباً بك في عالم البرمجة! ");
+
+/* key word is case sensitive so we should be careful to not use capital instead of small latter*/
