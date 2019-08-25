0b61d01addf6cbf991a5419684f4848ad4afe264
shhadr <shahadahmad18@gmail.com>
2019-08-25 16:12:57 +0300

    day6

---
+++ Day6.js
@@ -0,0 +1,17 @@
+// first example
+function test(){
+  document.write("Hello world!<br>")
+  document.write("We will try to learn about the identifires")
+}
+
+test()
+
+document.write("<br>")
+
+// second example
+function test1(){
+  let name = "Shahad"
+  document.write("My name is"+ " " + name)
+}
+
+test1()
