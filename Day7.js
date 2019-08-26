ebee7b611f41555d760914efa9d89c741a8ab5eb
shhadr <shahadahmad18@gmail.com>
2019-08-26 21:26:13 +0300

    day7

--- Day7.js
+++ Day7.js
@@ -1,5 +1,5 @@
 function Student(){
-    let __case ="software enginering" // we add special character with the var name because it's reserved word 
+    let __case ="software enginering" // I add special character with the var name because "case" is reserved word 
     
     let My_class = "7W1"
     document.write( "I have"+ " " + __case + " " +"class and my section is" + " " + My_class )
