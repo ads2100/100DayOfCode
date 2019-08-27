9173d28186aef0e34700e17f53937cbb58168609
shhadr <shahadahmad18@gmail.com>
2019-08-27 20:18:10 +0300

    day8

--- Day7.js
+++ Day7.js
@@ -1,8 +1,8 @@
 function Student(){
-    let __case ="software enginering" // I add special character with the var name because "case" is reserved word 
-    
-    let My_class = "7W1"
-    document.write( "I have"+ " " + __case + " " +"class and my section is" + " " + My_class )
-  }
+  let __case ="software enginering" // I add special character with the var name because "case" is reserved word 
   
-  Student()
+  let class = "7W1" // this will not printed because "class" is reserved word
+  document.write( "I have"+ " " + __case + " " +"class and my section is" + " " + class )
+}
+
+Student()
---
+++ Day8.js
@@ -0,0 +1,12 @@
+let a = 18;
+let b = 2;
+let result = a % b;
+
+document.write("The division remainder of" +  " " + a + " " + "modulu" + " " + b + " " + "is" + " " + result);
+
+// second code
+
+document.write("<br>");
+result = a !== "18" ;
+
+document.write("the result of 18 is not equal value or not equal type to '18' is :" + " " + result ) ;
