f531bc134171af4574b3a39f9429182aa0700770
shhadr <shahadahmad18@gmail.com>
2019-09-06 18:23:51 +0300

    Day18and19

--- Week3/Day.js
+++
@@ -1,3 +0,0 @@
-var Day = "wednesday"; switch(Day) { case "sunday": case "monday":
-17
-case "tuesday": case "wednesday": case "thursday": document.write("it is work days"); break; default: document.write("Happy weekend"); break; }
