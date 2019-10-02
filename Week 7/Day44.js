9f66ec78c2e82ddbafba27f9fcf30ddf29e1bee2
shhadr <shahadahmad18@gmail.com>
2019-10-02 19:58:43 +0300

    week7

---
+++ Week 7/Day44.js
@@ -0,0 +1,9 @@
+// there is alot of ways to create new array
+
+var cars = ["Jeep", "Honda", "Audi"]; // this is the first way
+
+var Cars = new Array("Jeep", "Honda", "Audi"); // this is the second one
+
+console.log(Cars[0]); // and we can acsses the array as usual way 
+console.log(Cars[3]);
+console.log("My car is " + Cars[1]);
---
+++ Week 7/Day45.js
@@ -0,0 +1,5 @@
+var nationality = ["Saudi", "English", "Egyption"];
+var nat = nationality[2];
+console.log(nat);
+console.log(nationality.length); // this will print the length of this array
+console.log(nationality[4]); // will print in the console "undefined" because the number of index in this array is 2 
